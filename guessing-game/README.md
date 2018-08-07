62c7029881a463432a0752593e24cabd90a2e63d
budi <budioppa@hotmail.com>
2018-08-06 19:14:37 -0700

    initiating and getting Ref to Gtk and start app

---
+++ guessing-game/README.md
@@ -0,0 +1,39 @@
+﻿## A Guessing game 
+
+
+## Installation :
+
+The **gtk** crate expects **GTK+**, **GLib** and **Cairo** development files to be installed on your system.
+
+**For Debian**
+```
+sudo apt-get install libgtk-3-dev
+```
+**For fedora**
+```
+sudo dnf install gtk3-devel glib2-devel
+```
+To compile and run
+```
+cargo run 
+```
+##  About this game :
+
+ - This is just a simple gtk rs based guessing game .  The Game start
+   and Randomize a I32 variable **X**.  So when user type an integer >
+   **X**  then event is fired and an text box setTetx() is changed to **TOO big**
+ - Then if user input is less then **X**  then changes value of text field to Too small .
+
+So using basic equality rules , User guess **the actual value ** using uper limit and lower limit of the variable 
+
+## How did it went  ?
+Firstly , I didn't go as planned . I had alot of problems with libraries that wasn't build passing . So I had to stick to **rand**  crate . 
+
+Firstly I was going to use Glade prototyping my application then I changed my mind , go for hardcoding each label , button in the rust code . 
+
+## Review about gtk rs
+
+ - Since gtk rs dosn't uses any strict rules about ownership and Borrowing . The lib handles these situation itself aslo I didn't have to write a 100 lines of gtk code. So , I was happy that majority of the trait impl I had to do using Rust . 
+
+
+
---
+++ guessing-game/src/main.rs
@@ -0,0 +1,96 @@
+extern crate gtk;
+extern crate gio;
+extern crate rand;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+#[derive(Debug)]
+enum Mode {
+	Normal,
+	Hard
+}
+
+struct HeaderUi {
+	headerbar: gtk::HeaderBar,
+	switch: gtk::Switch,
+	start_button: gtk::Button
+}
+
+impl HeaderUi {
+	fn new() -> HeaderUi {
+		let headerbar = gtk::HeaderBar::new();
+		headerbar.set_title("Guessing Game");
+		headerbar.set_show_close_button(true);
+
+		let switch = gtk::Switch::new();
+		let start_button = gtk::Button::new_with_label("Start");
+
+		headerbar.pack_start(&switch);
+		headerbar.pack_end(&start_button);
+
+		HeaderUi { headerbar, switch, start_button }
+	}
+}
+
+fn ui(app: &gtk::Application) {
+	let secret: Rc<RefCell<Option<u32>>> = Rc::new(RefCell::new(None));
+	let mode: Rc<RefCell<Mode>> = Rc::new(RefCell::new(Mode::Normal));
+	let window = gtk::ApplicationWindow::new(app);
+	let header = HeaderUi::new();
+	let switch = header.switch;
+	let start_button = header.start_button;
+
+	// Window HeaderBar
+	window.set_titlebar(&header.headerbar);
+
+	// (width, height);
+	window.set_default_size(600, 300);
+
+	// (orientation, spacing)
+	let container = gtk::Box::new(
+		gtk::Orientation::Vertical,
+		0
+	);
+	container.set_sensitive(false);
+
+	// Message
+	let message_box = gtk::Box::new(
+		gtk::Orientation::Horizontal,
+		0
+	);
+	message_box.set_halign(gtk::Align::Center);
+	let message = gtk::Label::new("Let's play");
+
+	// (widget, expand, fill, padding)
+	// Pack message
+	message_box.pack_start(
+		&message,
+		false,
+		false,
+		0
+	);
+
+	// Entry
+
+}
+
+fn main() {
+	let app = gtk::Application::new(
+		"com.github.guessing_game",
+		gio::ApplicationFlags::empty()
+	).expect("Failed..");
+
+	app.connect_startup(|app| {
+		ui(&app);
+	});
+
+	//run app
+	app.run(&std::env::args().collect::<Vec<_>>());
+}
